108a37bcb2366ecf859cb49afbb52bf8f654789d
Livien Yin <LivienYin@gmail.com>
2013-04-22 23:36:38 -0700

    Moved action mailer configuration from development only config to general config.

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,17 @@ EarshareApp::Application.initialize!
 
 EarshareApp::Application.configure do
   config.last_fm_api_key = ENV["LAST_FM_API_KEY"]
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587, 
+    :domain => "mysite.com",
+    :user_name => "livienyin",
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+  }
+  # DO care
+  config.action_mailer.raise_delivery_errors = true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,17 +16,7 @@ EarshareApp::Application.configure do
   # DO care
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587, 
-    :domain => "mysite.com",
-    :user_name => "livienyin",
-    :password => ENV['GMAIL_PASSWORD'],
-    :authentication => "plain",
-    :enable_starttls_auto => true,
-  }
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
